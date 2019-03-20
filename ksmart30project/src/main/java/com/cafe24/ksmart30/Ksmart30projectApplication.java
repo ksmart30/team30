c9b7b9596cbed79a2fe126aeba9dfada4c5b2127
smart-09 <smart-09@smart-09-PC>
2019-03-20 16:45:40 +0900

    [ksamrt30] team project 생성

---
+++ ksmart30project/src/main/java/com/cafe24/ksmart30/Ksmart30projectApplication.java
@@ -0,0 +1,13 @@
+package com.cafe24.ksmart30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ksmart30projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ksmart30projectApplication.class, args);
+	}
+
+}
