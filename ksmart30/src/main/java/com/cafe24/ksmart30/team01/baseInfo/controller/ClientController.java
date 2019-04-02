1593d27d7a8a4767bc16c998111af4ee1818f3c7
yidaeun39 <yde0309@gmail.com>
2019-04-02 17:47:15 +0900

    [team01] 누락된 파일 업로드 - ClientController.java - BusinessManageController.java - BusinessYesanController.java - BusinessYesanController.java

---
+++ ksmart30/src/main/java/com/cafe24/ksmart30/team01/baseInfo/controller/ClientController.java
@@ -0,0 +1,59 @@
+package com.cafe24.ksmart30.team01.baseInfo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import groovyjarjarpicocli.CommandLine.Model;
+
+@Controller
+public class ClientController {
+
+	
+	//0.1 거래처코드 등록
+	@GetMapping("/baseinfo/ClientService/in")
+	public String baseInfoClientIn() {
+		return "baseInfo/client/in";
+	}
+	
+	//0.2 거래처코드 리스트 출력
+	@GetMapping("/baseinfo/ClientService/list")
+	public String baseInfoClientList() {
+		return "baseInfo/ClientService/list";
+	}
+	
+	
+	//0.2 거래처코드 리스트 출력
+	@PostMapping("/baseinfo/ClientService/list")
+	public String baseInfoClientList(Model model) {
+		return "baseInfo/ClientService/list";
+	}
+
+	
+/*	//1.2 거래처 입력 처리
+	@PostMapping("/baseInfo/ClientService/in")
+	public String baseInfoClientIn(Model model) {
+		return "baseInfo/ClientService/in";
+	}
+	
+	//1.3 거래처 수정 화면
+	@GetMapping("/baseInfo/ClientService/up")
+	public String baseInfoClientUp() {
+		return "/baseInfo/ClientService/up";
+	}
+	
+	//1.3 거래처 수정 처리
+	@PostMapping("/baseInfo/ClientService/up")
+	public String baseInfoClientUp(Model model) {
+		return "/baseInfo/ClientService/up";
+	}
+	
+	//1.4 거래처 삭제 처리
+	@GetMapping("/baseInfo/ClientService/del")
+	public String baseInfoClientDel() {
+		return "baseInfo/ClientService/del";
+		
+		}
+		*/
+	
+}
--- ksmart30/src/main/java/com/cafe24/ksmart30/team01/business/controller/BusinessYesanController.java
+++ ksmart30/src/main/java/com/cafe24/ksmart30/team01/business/controller/BusinessYesanController.java
@@ -7,108 +7,42 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BusinessYesanController {
-	
-	//2.2.1 승인된 사업수행 결정서 검색 List
-	@GetMapping("/business/yesan/search")
-	public String businessYesanSearch() {
 		
-		
-		return "business/yesan/search";	
-	}
-	
-	//2.2.1 승인된 사업수행 결정서 검색 List
-	@PostMapping("/business/yesan/search")
-	public String businessYesanSearch(Model model) {
-		
-		return "business/yesan/search";
-	}
-	
-	//2.2.1 승인된 사업수행 결정서 예산 입력 화면만 보여주는 메서드
+	//1.2.1 사업성검토 예산 입력
 	@GetMapping("/business/yesan/in")
 	public String businessYesanIn() {
 		
 		return "business/yesan/in";	
-	}
-	
-	//2.2.1 승인된 사업수행 결정서 일정계획 수정 처리
-	@PostMapping("business/yesan/in_schedule_up")
-	public String businessYesanInScheduleUp() {
+	}	
 		
-		return "business/yesan/in_schedule_up";
-	}
-	
-	//2.2.1 승인된 사업수행 결정서 제조경비1 입력 처리
-	@PostMapping("/business/yesan/in_jejo_one_in")
-	public String businessYesanInJejoOneIn() {
-		
-		return "bbusiness/yesan/in_jejo_one_in";
-	}
-	
-	//2.2.1 승인된 사업수행 결정서 제조경비2 입력 처리
-	@PostMapping("/business/yesan/in_jejo_two_in")
-	public String businessYesanInJejoTwoIn() {
-		
-		return "business/yesan/in_jejo_two_in";
-	}
-	
-	//2.2.1 승인된 사업수행 결정서 예산 승인을 위한 List
-	@GetMapping("/business/yesan/seungin_search")
-	public String businessYesanSeunginSearch() {
-		
-		return "business/yesan/seungin_search";
-	}
-	
-	//2.2.1 승인된 사업수행 결정서 예산 승인을 위한 List
-	@PostMapping("/business/yesan/seungin_search")
-	public String businessYesanSeunginSearch(Model model) {
-		
-		return "business/yesan/seungin_search";	
-	}
-	
-	
-	//2.2.2 승인된 사업수행 결정서 예산 승인
+	//1.2.2 사업성검토 예산 승인
 	@GetMapping("/business/yesan/seungin")
 	public String businessYesanSeungin() {
 		
 		return "business/yesan/seungin";
 	}
-	
-	//2.2.2 승인된 사업수행 결정서 예산 승인
-	@PostMapping("/business/yesan/seungin")
-	public String businessYesanSeungin(Model model) {
 		
-		return "business/yesan/seungin";
-	}
-	
-	//2.2.3 승인된 승인 사업수행 결정서 예산 출력
+	//1.2.3 사업성검토 예산 출력
 	@GetMapping("/business/yesan/output")
 	public String businessYesanOutput() {
 		
 		return "business/yesan/output";
 	}
-	
-	//2.2.3 승인된 승인 사업수행 결정서 예산 출력
-	@PostMapping("/business/yesan/output")
-	public String businessYesanOutput(Model model) {
 		
-		return "business/yesan/output";
+	//1.2.4 사업성검토 검색1
+	@GetMapping("/business/yesan/search")
+	public String businessYesanSearch() {
+				
+		return "business/yesan/search";	
 	}
 	
-	//2.2.4 승인된 승인 사업성검토 검색 List
-	@GetMapping("/business/yesan/list")
-	public String businessYesanList() {
+	//1.2.5 사업성검토 검색2
+	@GetMapping("business/yesan/search2")
+	public String businessyesansearch2() {
 		
-		return "business/yesan/list";
+		return "business/yesan/search2";
 	}
 
-	//2.2.4 승인된 승인 사업성검토 검색 List
-	@PostMapping("/business/yesan/list")
-	public String businessYesanList(Model model) {
-		
-		return "business/yesan/list";
-	}
 }
-
-
 
 
